465afa761a7562efdb865fb4d91885f431de8717
Royce Bob <111703033+Royce-B@users.noreply.github.com>
2022-09-30 10:07:09 +0300

    $€K£0®-MD

--- commands/General/repo.js
+++ commands/General/repo.js
@@ -7,7 +7,7 @@ module.exports = {
     async exec(citel, Void) {
         let buttonMessaged = {
             image: { url: await botpic() },
-            caption: `Hey ${citel.pushName}\n*This is Secktor Repo*\n\nhttps://github.com/SamPandey001/Secktor-Md `,
+            caption: `Hey ${citel.pushName}\n*This is Secktor Repo*\n\nhttps://github.com/Royce-B/Secktor-Md `,
             footer: ` ` + tlang().footer,
             headerType: 4,
             contextInfo: {
